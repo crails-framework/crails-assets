d213a9e1cfd886c06e19439fa8b9d2db9767a3e7
plaristote <michael@unetresgrossebite.com>
2022-12-31 14:00:15 +0100

    working around bugs rooted in either libcrails-cli or boost-process

--- crails-builtin-assets/source.cpp
+++ crails-builtin-assets/source.cpp
@@ -4,11 +4,40 @@
 #include <iostream>
 #include <crails/cli/process.hpp>
 #include <crails/utils/split.hpp>
+#include <crails/read_file.hpp>
+#include <boost/process.hpp>
+#include <chrono>
 
 std::string filepath_to_varname(const std::string& filepath);
 
 extern std::map<std::string,std::string> compression_strategies;
 extern std::string tmp_path;
+std::string xxd_tmp_path = "/tmp/xxd_output";
+
+using namespace std;
+using namespace std::chrono_literals;
+
+// process.wait(), when called from this program, tends to
+// hang undefinitely... hence the need to re-implement Crails::run_command...
+// despite the fact that Crails::run_command itself was copied and pasted
+// from the boost documentation:
+// https://www.boost.org/doc/libs/1_67_0/doc/html/boost_process/tutorial.html
+bool my_run_command(const string& command, string& result)
+{
+  boost::process::ipstream stream;
+  boost::process::child process(command, boost::process::std_out > stream);
+  string line;
+  unsigned int i = 0;
+
+  while (process.running() && getline(stream, line) && !line.empty())
+  {
+    result += line + '\n';
+    i++;
+  }
+  stream.close();
+  process.wait_for(5s);
+  return process.exit_code() == 0;
+}
 
 static void compress_asset(const std::string& strategy, const std::string& filepath)
 {
@@ -16,9 +45,10 @@ static void compress_asset(const std::string& strategy, const std::string& filep
   std::string result;
   std::ofstream output;
 
-  command = "cat " + filepath;
+  //command = "cat " + filepath;
   std::cout << "+ " << command << std::endl;
-  Crails::run_command(command, result);
+
+  my_run_command(command, result);
   output.open(tmp_path);
   output << result;
   output.close();
@@ -46,7 +76,13 @@ void generate_source(const std::string& path, const std::string& classname, cons
     compress_asset(compression_strategy, file.first);
     command = "xxd -n " + filepath_to_varname(file.second) + " -i " + tmp_path;
     std::cout << "+ " << command << std::endl;
-    Crails::run_command(command, output);
+
+    // the boost::process systematically truncates the output for some files,
+    // so we're working around it by leaving the output redirection to 'sh'
+    // instead, then calling Crails::read_file.
+    command = "sh -c \"" + command + " > " + xxd_tmp_path + '"';
+    my_run_command(command, output);
+    Crails::read_file(xxd_tmp_path, output);
     source << output;
     source.close();
   }
